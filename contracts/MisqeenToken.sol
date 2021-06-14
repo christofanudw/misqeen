cadb2bbca71f875da9eb7844ed0cbb0f4d4be1a3
christofanudw <chrstfd98@gmail.com>
2021-06-14 15:27:21 +0200

    Added delegated transfer functionality

--- contracts/MisqeenToken.sol
+++ contracts/MisqeenToken.sol
@@ -11,8 +11,12 @@ contract MisqeenToken{
     uint256 public totalSupply;
     // Balance of
     mapping(address => uint256) public balanceOf;
+    // allowance state variable
+    mapping(address => mapping(address => uint256)) public allowance;
     // Transfer event
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    // allowance
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
     // Constructor
     constructor(uint256 _initialSupply) public{
         // Assigning all initial token supply to contract deployer
@@ -31,5 +35,25 @@ contract MisqeenToken{
         // Return a boolean
         return true;
     }
-    // Read total number of tokens 
+    // Delegated Transfer
+    // approves _owner to spend _value amount of token on our behalf
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        // alllowance
+        allowance[msg.sender][_spender] = _value;
+        // approve event
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+        require(balanceOf[_from] >= _value);
+        require(allowance[_from][msg.sender] >= _value);
+        // Transfer token
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        // fire event
+        emit Transfer(_from, _to, _value);
+        // return
+        return true;
+    }
 }

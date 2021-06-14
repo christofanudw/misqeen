0d6725bc8b1ea696691b9cc0f224148ecaf32397
christofanudw <chrstfd98@gmail.com>
2021-06-14 17:53:37 +0200

    Added ICO contract to handle the sale of the tokens

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 const MisqeenToken = artifacts.require("./MisqeenToken.sol");
+const MisqeenICO = artifacts.require("./MisqeenICO.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(MisqeenToken, 1000000);
+  deployer.deploy(MisqeenToken, 1000000)
+    .then(function(){
+      return deployer.deploy(MisqeenICO, MisqeenToken.address, 1e15);
+    });
+  
 };
